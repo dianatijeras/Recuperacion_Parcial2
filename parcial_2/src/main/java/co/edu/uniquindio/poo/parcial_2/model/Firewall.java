d9ae00cc76b33e9783d36c90f5efc2cc7c7701b8
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2025-04-30 19:08:13 -0500

    creacion proyecto

---
+++ parcial_2/src/main/java/co/edu/uniquindio/poo/parcial_2/model/Firewall.java
@@ -0,0 +1,28 @@
+package co.edu.uniquindio.poo.model;
+
+/**
+ * Módulo decorador que añade funcionalidad de firewall.
+ */
+public class Firewall extends ModuloDecorator {
+
+    public Firewall(Dispositivo decorado) {
+        super(decorado);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String estado() {
+        return decorado.estado() + " + Módulo: Firewall";
+    }
+
+    @Override
+    public int prioridad() {
+        return decorado.prioridad() + 2;
+    }
+
+    @Override
+    public String tipo() {
+        return decorado.tipo();
+    }
+    
+}
---
+++ parcial_2/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.parcial_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.parcial_2 to javafx.fxml;
+    exports co.edu.uniquindio.poo.parcial_2;
+}
